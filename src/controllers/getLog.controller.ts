62700caee4d9490cafb200cf7e8167ef29abdb8f
Nbinn <nhantt2001@gmail.com>
2022-11-28 19:11:13 +0700

    feat:login

--- src/controllers/getLog.controller.ts
+++ src/controllers/getLog.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import pool from '$/config/db';
 
-export const getUser = (req: Request, res: Response) => {
+export const getLog = (req: Request, res: Response) => {
     pool.query('SELECT * FROM fabric_store', (err, data) => {
         if(err) {
             console.log(err)
