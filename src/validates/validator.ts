d00fe82908563954599987894edabcc905396602
Nbinn <nhantt2001@gmail.com>
2022-12-02 01:44:30 +0700

    fix Log

--- src/validates/validator.ts
+++ src/validates/validator.ts
@@ -9,9 +9,7 @@ export const logValidation = () => {
 export const validate = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req)
     if (errors.isEmpty()) {
-      if (req.body.partner_id) {
-        req.body.partner_id = parseInt(req.body.partner_id)
-      }
+      
       return next()
     }
     res.status(400).json({
