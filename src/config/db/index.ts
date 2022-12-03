6c5f32c0296d5a07f36797bd3afc1f368b4894a1
angushhz <hoangnamqt.2001@gmail.com>
2022-12-03 11:25:57 +0700

    fix:detail catagories

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -2,15 +2,29 @@ import mysql from 'mysql2'
 import dotenv from 'dotenv'
 
 dotenv.config()
+let option = {}
+console.log('App is running in environment: ', process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+  option = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'fabric_store',
+    port: 3306,
+    waitForConnections: true
+  }
+} else if (process.env.NODE_ENV === 'production') {
+  option = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    waitforconnections: true
+  }
+}
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: 5771,
-  waitForConnections: true
-})
+const pool = mysql.createPool(option)
 
 pool.getConnection((err, connection) => {
   try {
